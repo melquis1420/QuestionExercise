5cdcfe2a6971a9f8c6226c7df620d97a0094d5e4
melquis1420 <46160910+melquis1420@users.noreply.github.com>
2021-11-12 11:24:32 -0300

    Map on main
    Convert String list to Map.

--- lib/main.dart
+++ lib/main.dart
@@ -16,16 +16,26 @@ class _PerguntaAppState extends State<PerguntaApp> {
 
   @override
   Widget build(BuildContext context) {
-    final perguntas = [
-      'Qual é a sua cor favorita?',
-      'Qual é o seu animal favorito?',
+    final List<Map<String, Object>> perguntas = [
+      {
+        'texto': 'Qual a sua cor favorita? ', //string
+        'respostas': ['Preto', 'Branco', 'Azul', 'Amarelo'], //object
+      },
+      {
+        'texto': 'Qual o seu animal favorito?',
+        'respostas': ['Dragão de comodo', 'Arara', 'Barata', 'Dinossauro']
+      },
+      {
+        'texto': 'Qual o seu instrutor favorito?',
+        'respostas': ['Echo', 'Pedro', 'Thiago', 'João']
+      }
     ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('Perguntas')),
         body: Column(
           children: <Widget>[
-            Questao(perguntas[_perguntaSelecionada]),
+            Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
             Resposta('Resposta 1', _responder),
             Resposta('Resposta 2', _responder),
             Resposta('Resposta 3', _responder),
